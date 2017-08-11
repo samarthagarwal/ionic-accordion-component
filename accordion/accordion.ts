d0e9572fe394a2189f58381719d952d18d1aaa80
unknown <samarthagarwal@live.com>
2017-08-11 23:00:21 +0530

    Initial

---
+++ accordion/accordion.ts
@@ -0,0 +1,46 @@
+import { Component, ViewChild, OnInit, Renderer, Input } from '@angular/core';
+
+/**
+ * Generated class for the AccordionComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
+@Component({
+  selector: 'accordion',
+  templateUrl: 'accordion.html'
+})
+export class AccordionComponent implements OnInit {
+
+  accordionExapanded = false;
+  @ViewChild("cc") cardContent: any;
+  @Input('title') title: string;
+
+  icon: string = "arrow-forward";
+
+  constructor(public renderer: Renderer) {
+
+  }
+
+  ngOnInit() {
+    console.log(this.cardContent.nativeElement);
+    this.renderer.setElementStyle(this.cardContent.nativeElement, "webkitTransition", "max-height 500ms, padding 500ms");
+  }
+
+  toggleAccordion() {
+    if (this.accordionExapanded) {
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height", "0px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding", "0px 16px");
+
+    } else {
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "max-height", "500px");
+      this.renderer.setElementStyle(this.cardContent.nativeElement, "padding", "13px 16px");
+
+    }
+
+    this.accordionExapanded = !this.accordionExapanded;
+    this.icon = this.icon == "arrow-forward" ? "arrow-down" : "arrow-forward";
+
+  }
+
+}
